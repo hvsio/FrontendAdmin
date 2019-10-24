6a04ad52833450954c69f99853425b1bc1a0a49e
patrick <patrickvibild@gmail.com>
2019-10-25 01:51:18 +0200

    Merge branch 'master' of /home/patrick/WebstormProjects/frontend-admin with conflicts.

--- src/app/bank.service.ts
+++ src/app/bank.service.ts
@@ -3,26 +3,27 @@ import {HttpClient} from "@angular/common/http";
 import {Observable, of, pipe} from "rxjs";
 import {Bank} from './bank';
 import {BANKS} from './list';
-import { catchError, map, tap } from 'rxjs/operators';
-import { delay } from 'rxjs/internal/operators';
-import { ControlContainer } from '@angular/forms';
+import {catchError, map, tap} from 'rxjs/operators';
+import {delay} from 'rxjs/internal/operators';
+import {ControlContainer} from '@angular/forms';
 
 
 const SERVER_URL: string = 'http://localhost:8000/banks';
 
 @Injectable({
   providedIn: 'root',
-}) 
+})
 export class BankService {
-  
-  constructor (private http:HttpClient) { }
-  
+
+  constructor(private http: HttpClient) {
+  }
+
 
   getBank(bankId: number) {
     return this.http.get(`${SERVER_URL}/${bankId}`)
-    .pipe(
-      catchError(this.handleError<any>('data'))
-    );
+      .pipe(
+        catchError(this.handleError<any>('data'))
+      );
   }
 
   // getBanks(): Observable<Bank[]> {
@@ -36,53 +37,62 @@ export class BankService {
   //       console.log(BANKS);
   //     }
   //   )
-    
+
   //   return of (BANKS);
   // }
 
-    getBanks() {
-      return this.http.get(`${SERVER_URL}`)
+  getBanks() {
+    return this.http.get(`${SERVER_URL}`)
   }
-  
 
-  deleteBank(bankId: string) {
-    console.log(`${SERVER_URL}/${bankId}`)
+
+  deleteBank(bankId: number) {
     return this.http.delete(`${SERVER_URL}/${bankId}`)
+      .pipe(
+        catchError(this.handleError<any>('data'))
+      );
   }
 
-  postBank(name:string, country:string, pageurl:string, fromcurrency:string, tocurrencyxpath:string, buyxpath:string, sellxpath:string, unit: string) {
-  return this.http.post(`${SERVER_URL}`, 
-  {
-    "name": name,
-    "country": country,
-    "pageurl": pageurl,
-    "fromcurrency": fromcurrency,
-    "tocurrencyxpath": tocurrencyxpath,
-    "buyxpath": buyxpath,
-    "sellxpath": sellxpath,
-    "unit": unit,
- 
-  }).pipe(
-    catchError(this.handleError<any>('data'))
-  );
-}
-
-  putBank(bank: Bank) {
-    return this.http.put(`${SERVER_URL}`, bank
-    ).pipe(
+  postBank(name: string, country: string, pageurl: string, fromcurrency: string, tocurrencyxpath: string,
+           buyxpath: string, sellxpath: string, unit: string) {
+    return this.http.post(`${SERVER_URL}`,
+      {
+        "name": name,
+        "country": country,
+        "pageurl": pageurl,
+        "fromcurrency": fromcurrency,
+        "tocurrencyxpath": tocurrencyxpath,
+        "buyxpath": buyxpath,
+        "sellxpath": sellxpath,
+        "unit": unit
+      }).pipe(
       catchError(this.handleError<any>('data'))
     );
   }
 
-  private handleError<T> (operation = 'operation', result?: T) {
+  putBank() {
+    return this.http.put(`${SERVER_URL}`, {
+      "name": " ",
+      "country": " ",
+      "pageurl": " ",
+      "fromcurrency": " ",
+      "tocurrencyxpath": " ",
+      "buyxpath": " ",
+      "sellxpath": " "
+    }).pipe(
+      catchError(this.handleError<any>('data'))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-  
+
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
-  
+
       // TODO: better job of transforming error for user consumption
       console.log(`${operation} failed: ${error.message}`);
-  
+
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
