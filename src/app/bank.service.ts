a535b917e739f38b6985a72b5c0394433efa5c4f
hvsio <h4sio@o2.pl>
2019-10-29 13:10:40 +0100

    lol

--- src/app/bank.service.ts
+++ src/app/bank.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable, of, pipe} from "rxjs";
 import {Bank} from './bank';
-<<<<<<< HEAD
 import { catchError, map, tap } from 'rxjs/operators';
 import { delay } from 'rxjs/internal/operators';
 import { ControlContainer } from '@angular/forms';
@@ -13,22 +12,11 @@ import { environment } from 'src/environments/environment.prod';
 
 const SERVER_URL: string = environment.scrapperConfig + '/banks';
 
-=======
-import {BANKS} from './list';
-import {catchError, map, tap} from 'rxjs/operators';
-import {delay} from 'rxjs/internal/operators';
-import {ControlContainer} from '@angular/forms';
-import { environment } from '../environments/environment';
-
-const baseUrl = environment.scrapperConfig;
-const SERVER_URL: string = baseUrl + '/banks';
->>>>>>> f4ebeab31cd72d6175eeab76397f92774a38325e
 
 @Injectable({
   providedIn: 'root',
 })
 export class BankService {
-<<<<<<< HEAD
   
   constructor (private http:HttpClient, private snackBar: MatSnackBar) { }
 
@@ -38,12 +26,6 @@ export class BankService {
     });
   }
   
-=======
-
-  constructor(private http: HttpClient) {
-  }
-
->>>>>>> f4ebeab31cd72d6175eeab76397f92774a38325e
 
   getBank(bankId: number) {
     return this.http.get(`${SERVER_URL}/${bankId}`)
@@ -52,28 +34,8 @@ export class BankService {
       );
   }
 
-<<<<<<< HEAD
     getBanks() {
       return this.http.get(`${SERVER_URL}`)
-=======
-  // getBanks(): Observable<Bank[]> {
-  //   this.http.get(`${SERVER_URL}`).subscribe (
-  //     (data:Bank[]) => {
-  //       for (let i in data) {
-  //         var bankie = new Bank(data[i].name, data[i].country, data[i].pageurl, data[i].buyxpath, data[i].sellxpath, data[i].fromCurrency, data[i].toCurrencyXpath);
-  //         BANKS.push(bankie);
-  //         console.log("Bank service is working");
-  //       }
-  //       console.log(BANKS);
-  //     }
-  //   )
-
-  //   return of (BANKS);
-  // }
-
-  getBanks() {
-    return this.http.get(`${SERVER_URL}`)
->>>>>>> f4ebeab31cd72d6175eeab76397f92774a38325e
   }
 
 
@@ -84,7 +46,6 @@ export class BankService {
       );
   }
 
-<<<<<<< HEAD
   postBank(name:string, country:string, pageurl:string, fromcurrency:string, tocurrencyxpath:string, buyxpath:string, sellxpath:string, unit: string) {
   return this.http.post(`${SERVER_URL}`, 
   {
@@ -114,35 +75,6 @@ export class BankService {
     "unit": bank.unit 
     }
     ).pipe(
-=======
-  postBank(name: string, country: string, pageurl: string, fromcurrency: string, tocurrencyxpath: string,
-           buyxpath: string, sellxpath: string, unit: string) {
-    return this.http.post(`${SERVER_URL}`,
-      {
-        "name": name,
-        "country": country,
-        "pageurl": pageurl,
-        "fromcurrency": fromcurrency,
-        "tocurrencyxpath": tocurrencyxpath,
-        "buyxpath": buyxpath,
-        "sellxpath": sellxpath,
-        "unit": unit
-      }).pipe(
-      catchError(this.handleError<any>('data'))
-    );
-  }
-
-  putBank() {
-    return this.http.put(`${SERVER_URL}`, {
-      "name": " ",
-      "country": " ",
-      "pageurl": " ",
-      "fromcurrency": " ",
-      "tocurrencyxpath": " ",
-      "buyxpath": " ",
-      "sellxpath": " "
-    }).pipe(
->>>>>>> f4ebeab31cd72d6175eeab76397f92774a38325e
       catchError(this.handleError<any>('data'))
     );
   }
@@ -161,7 +93,6 @@ export class BankService {
     };
   }
 
-<<<<<<< HEAD
   private handleAddingError<T> (operation = 'operation', message: string, action: string, result?: T) {
     return (error: any): Observable<T> => {
   
@@ -178,6 +109,3 @@ export class BankService {
   }
 
 }
-=======
-}
->>>>>>> f4ebeab31cd72d6175eeab76397f92774a38325e
