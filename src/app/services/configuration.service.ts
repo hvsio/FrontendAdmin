c5407deabc414dbee4dca4764ddf7c005c22b378
hvsio <h4sio@o2.pl>
2019-12-06 13:46:29 +0100

    not-clean final

--- src/app/services/configuration.service.ts
+++ src/app/services/configuration.service.ts
@@ -60,47 +60,47 @@ export class ConfigurationService {
 
   //CURRENCIES
 
-  getCurrencies(): Observable<Currency[]> {
-    return this.http.get<Currency[]>(`${currencyUrl}`);
-  }
-
-  deleteCurrency(currency: Currency): Observable<Currency> {
-    const url = `${currencyUrl}/${currency.id}`;
-    return this.http.delete<Currency>(url, httpOptions);
-  }
-
-  addCurrency(currency: any): Observable<any> {
-    return this.http.post<any>(currencyUrl, currency, httpOptions).pipe(
-      catchError(this.handleError<any>('data'))
-    );
-  }
-
-  updateCurrency(currency: Currency): Observable<any> {
-    const url = `${currencyUrl}`;
-    return this.http.put(url, currency, httpOptions);
-  }
-
-  //COUNTRIES
-
-  getCountries(): Observable<Country[]> {
-    return this.http.get<Country[]>(`${countryUrl}`);
-  }
-
-  deleteCountry(country: Country): Observable<Country> {
-    const url = `${countryUrl}/${country.id}`;
-    return this.http.delete<Country>(url, httpOptions);
-  }
-
-  addCountry(country: any): Observable<any> {
-    return this.http.post<any>(countryUrl, country, httpOptions).pipe(
-      catchError(this.handleError<any>('data'))
-    );
-  }
-
-  updateCountry(country: Country): Observable<any> {
-    const url = `${countryUrl}`;
-    return this.http.put(url, country, httpOptions);
-  }
+  // getCurrencies(): Observable<Currency[]> {
+  //   return this.http.get<Currency[]>(`${currencyUrl}`);
+  // }
+  //
+  // deleteCurrency(currency: Currency): Observable<Currency> {
+  //   const url = `${currencyUrl}/${currency.id}`;
+  //   return this.http.delete<Currency>(url, httpOptions);
+  // }
+  //
+  // addCurrency(currency: any): Observable<any> {
+  //   return this.http.post<any>(currencyUrl, currency, httpOptions).pipe(
+  //     catchError(this.handleError<any>('data'))
+  //   );
+  // }
+  //
+  // updateCurrency(currency: Currency): Observable<any> {
+  //   const url = `${currencyUrl}`;
+  //   return this.http.put(url, currency, httpOptions);
+  // }
+  //
+  // //COUNTRIES
+  //
+  // getCountries(): Observable<Country[]> {
+  //   return this.http.get<Country[]>(`${countryUrl}`);
+  // }
+  //
+  // deleteCountry(country: Country): Observable<Country> {
+  //   const url = `${countryUrl}/${country.id}`;
+  //   return this.http.delete<Country>(url, httpOptions);
+  // }
+  //
+  // addCountry(country: any): Observable<any> {
+  //   return this.http.post<any>(countryUrl, country, httpOptions).pipe(
+  //     catchError(this.handleError<any>('data'))
+  //   );
+  // }
+  //
+  // updateCountry(country: Country): Observable<any> {
+  //   const url = `${countryUrl}`;
+  //   return this.http.put(url, country, httpOptions);
+  // }
 
   handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
